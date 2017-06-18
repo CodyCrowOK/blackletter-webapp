b3901f364fbc19204bf5d830c6cd179fddd359e1
Cody Crow <cody@cms07.org>
2017-06-18 12:01:38 -0500

    computer failing desperation commit

--- src/components/ui/Events.js
+++ src/components/ui/Events.js
@@ -1,12 +1,28 @@
 import React from 'react';
 
 const Events = props => {
-	const {events} = props;
+	// const {events} = props;
+	const events = [{'event':1,'name':'Big Wedding','owner':1}];
+
+	const noEvents = (
+		<div>You have not created any events yet. <a href="#">Create one.</a></div>
+	);
+
+	const eventList = () => {
+		const items = events.sort((a, b) => b.event > a.event);
+		const listItems = items.map((item, index) => <li key={index}>{item.name}</li>);
+		return <ul className="event-list">{listItems}</ul>;
+	};
 
 	return (
 		<div>
 			<h2>Events</h2>
 			<hr />
+			{
+				Array.isArray(events) && events.length > 0
+					? eventList()
+					: noEvents
+			}
 		</div>
 	);
 };
